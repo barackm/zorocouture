56f1604051520fc8095a8f84e9e8627a83fac2b6
Baraka mukelenga <baraka.m@code-care.pro>
2025-01-12 14:37:15 +0200

    feat: add redirects and clean up routing in next.config.ts for improved navigation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,63 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.zorocouture.com",
+          },
+        ],
+        destination: "https://zorocouture.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "zorocouture.com",
+          },
+        ],
+        missing: [
+          {
+            type: "header",
+            key: "x-forwarded-proto",
+            value: "https",
+          },
+        ],
+        destination: "https://zorocouture.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path+/",
+        destination: "/:path+",
+        permanent: true,
+      },
+      {
+        source: "/services.html",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/about.html",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/contact.html",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/portfolio.html",
+        destination: "/portfolio",
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
