4bf3c98a7e8dcb3a534b71013583cd0b6ef9a6dc
Baraka mukelenga <baraka.m@code-care.pro>
2025-01-12 14:53:32 +0200

    feat: simplify redirects in next.config.ts and update to TypeScript syntax

--- next.config.ts
+++ next.config.ts
@@ -1,63 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.zorocouture.com",
-          },
-        ],
-        destination: "https://zorocouture.com/:path*",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "zorocouture.com",
-          },
-        ],
-        missing: [
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "https",
-          },
-        ],
-        destination: "https://zorocouture.com/:path*",
-        permanent: true,
-      },
-      {
-        source: "/:path+/",
-        destination: "/:path+",
-        permanent: true,
-      },
-      {
-        source: "/services.html",
-        destination: "/services",
-        permanent: true,
-      },
-      {
-        source: "/about.html",
-        destination: "/about",
-        permanent: true,
-      },
-      {
-        source: "/contact.html",
-        destination: "/contact",
-        permanent: true,
-      },
-      {
-        source: "/portfolio.html",
-        destination: "/portfolio",
-        permanent: true,
-      },
-    ];
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
